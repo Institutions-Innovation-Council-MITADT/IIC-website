e65d3198ca84447e263918027ec655f4c85ed031
Vidhan Poddar <108071484+Gigamail@users.noreply.github.com>
2023-11-08 22:30:50 +0530

    footer changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,7 @@ import { useCallback } from "react";
 import 'animate.css'
 import { useNavigate } from "react-router-dom";
 import WOW from 'wowjs';
-
-
+import "../CSS/style.css"
 const About = () => {
   const navigate = useNavigate(); 
 
@@ -97,7 +96,53 @@ const About = () => {
         alt="Text"
         src="/aboutshapes2.png"
       />
-
+      <img
+      className="last_one"
+      alt="Text"
+      src="/lastpage.png"
+      />
+        <div className="footer">
+        <div className="line1"></div>
+        <div className="content">INSTITUTION’S INNOVATION COUNCIL</div>
+        <div className="line2"></div>
+        <div className="foot-content">
+            <div className="box1">
+                <div className="c1">Quick Links</div>
+                <div className="line3"></div>
+                <hr />
+                <div className="links">
+                   <div className="b1"><a href="">Home</a></div>
+                   <div className="b1"><a href="">About Us</a></div>
+                    <div className="b1"><a href="">Members</a></div>
+                    <div className="b1"><a href="">Events</a></div>
+                    <div className="b1"><a href="">Contact Us</a></div>
+                </div>
+            </div>
+            <div className="box2">
+                <div className="c2">Connect</div>
+                <div className="line3"></div>
+                <hr />
+                <div className="socials">
+                    <div className="b2"><i className="fa-brands fa-facebook-f "></i> </div>
+                    <div className="b3"><i className="fa-brands fa-instagram "></i></div>
+                    <div className="b4"><i className="fa-brands fa-linkedin-in "></i></div>
+                </div>
+            </div>
+            <div className="box3">
+                <div className="c3">Contact</div>
+                <div className="line3"></div>
+                <hr />
+                <div className="contact">
+                    <div className="box4">
+                        <b>Address :</b> New IT Building, N-602, MIT ADT University, Rajbaugh, Solapur - Pune Hwy, near Bharat Petrol Pump, Loni Kalbhor, Maharashtra 412201
+                    </div>
+                    <div className="box5">
+                        <b>Contact :</b> innovation@mituniversity.edu.in
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 };
